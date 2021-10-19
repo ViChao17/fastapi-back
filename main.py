ce2ac34db2996c659fc50918bd6e3e7617e9a8dc
Виктор <vitek_zema@mail.ru>
2021-10-20 01:56:21 +0300

    base backend

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print('hello world')
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+data = {
+    'xLabel': 'X',
+    'yLabel': 'Y',
+    'data': {
+        'A': 5,
+        'B': 10,
+        'C': 15
+    }
+}
+
+
+class Diagram(BaseModel):
+    xLabel: str
+    yLabel: str
+    data: dict
+
+
+app = FastAPI()
+
+
+@app.get("/", response_model=Diagram)
+async def read_root():
+    return data
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
