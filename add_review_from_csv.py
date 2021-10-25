c33e313ac86cdf243915bcb99d5cea113b8d5b93
Виктор <vitek_zema@mail.ru>
2021-10-25 10:43:52 +0300

    review add

--- add_review_from_csv.py
+++ add_review_from_csv.py
@@ -5,6 +5,7 @@ from fastapi import Depends
 import csv
 
 models.Base.metadata.create_all(bind=engine)
+item_type = ['Country', 'Year', 'ISO3166_alpha3', 'ISO3166_numeric', 'Region', 'SubRegion', 'OPEC', 'EU', 'OECD', 'CIS', 'Var', 'Value']
 
 
 def get_db():
@@ -16,7 +17,12 @@ def get_db():
 
 
 def add_item(db: Session, item):
-    pass
+    db_item = models.Review(Country=str(item[0]), Year=int(item[1]), Region=str(item[4]), SubRegion=str(item[5]),
+                            OPEC=bool(int(item[6])), EU=bool(int(item[7])), OECD=bool(int(item[8])), CIS=bool(int(item[9])),
+                            Var=str(item[10]), Value=float(item[11]))
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
 
 
 def set_review(db: Session):
@@ -24,6 +30,11 @@ def set_review(db: Session):
         reader = csv.reader(f, delimiter=",")
         en = enumerate(reader)
         print(en.__next__())
+        for i, item in en:
+            if i % 1000 == 0:
+                print(f'{i/2520}%')
+            add_item(db, item)
+        print('--End--')
 
 
 if __name__ == '__main__':
