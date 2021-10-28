e3b8c2d67b0557001e531f07791c20bfe4955087
Виктор <vitek_zema@mail.ru>
2021-10-28 07:39:27 +0300

    format for chart

---
+++ series_types.ts
@@ -0,0 +1,75 @@
+series: [
+  {
+    type: 'pie',
+    data: [
+      {name: 'A', y: 1, color: '#ffff00'},
+      {name: 'B', y: 2, color: '#393e46'},
+      {name: 'C', y: 3, color: '#00adb5'},
+      {name: 'D', y: 4, color: '#ff1f51'},
+      {name: 'E', y: 5, color: '#506ef9'},
+    ]
+  }
+]
+
+series: [
+  {
+    type: 'column',
+    data: [
+      {name: 'A', y: 1, color: '#ffff00'},
+      {name: 'B', y: 2, color: '#393e46'},
+      {name: 'C', y: 3, color: '#00adb5'},
+      {name: 'D', y: 4, color: '#ff1f51'},
+      {name: 'E', y: 5, color: '#506ef9'},
+    ]
+  }
+]
+
+series: [
+  {
+    type: 'pie',
+    data: [{
+      name: 'Chrome',
+      y: 61.41,
+      sliced: true
+    }, {
+      name: 'Internet Explorer',
+      y: 11.84
+    }, {
+      name: 'Firefox',
+      y: 10.85
+    }, {
+      name: 'Edge',
+      y: 4.67
+    }, {
+      name: 'Safari',
+      y: 4.18
+    }, {
+      name: 'Sogou Explorer',
+      y: 1.64
+    }, {
+      name: 'Opera',
+      y: 1.6
+    }, {
+      name: 'QQ',
+      y: 1.2
+    }, {
+      name: 'Other',
+      y: 2.61
+    }]
+  }
+]
+
+series: [{
+  name: 'Random data',
+  type: 'line',
+  data: [
+    {
+      x: 0,
+      y: 0
+    },
+    {
+      x: 1,
+      y: 2
+    }
+  ]
+}]
